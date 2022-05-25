1700a078643c370ba15c8d7b7b2dd524b5fb27ba
Rafel Nakashima <rafaelnakashima@rocketmail.com>
2022-05-25 17:46:13 -0300

    feat: adicionou classe PVE

---
+++ src/Battle/PVE.ts
@@ -0,0 +1,28 @@
+import Character from '../Character';
+import Fighter, { SimpleFighter } from '../Fighter';
+import Battle from './Battle';
+
+class PVE extends Battle {
+  private _environment: SimpleFighter[] | Fighter[];
+
+  constructor(
+    player: Character | Fighter,
+    environment: SimpleFighter[] | Fighter[],
+  ) {
+    super(player);
+    this._environment = environment;
+  }
+
+  fight(): number {
+    this._environment.forEach((enemy) => {
+      if (this.player.lifePoints > 0 || enemy.lifePoints > 0) {
+        this.player.attack(enemy);
+        enemy.attack(this.player);
+      }
+    });
+
+    return super.fight();
+  }
+}
+
+export default PVE;
